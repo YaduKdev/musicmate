c9b6197cc858a918ea335cff35b66b09be219ced
Yadu Krishnan <79215964+YaduKdev@users.noreply.github.com>
2025-06-24 20:36:53 +0530

    route bug test 7

--- musicmate-api/src/app.js
+++ musicmate-api/src/app.js
@@ -81,9 +81,9 @@ app.use("/api/stats", statsRouter);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../musicmate-view/dist")));
 
-  // app.get("*", (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, "../musicmate-view/dist/index.html"));
-  // });
+  app.get("/*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../musicmate-view/dist/index.html"));
+  });
 }
 
 //For Error Handling
